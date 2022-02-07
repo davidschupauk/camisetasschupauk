387be3554473e72fdaa75b04900bde87727efff5
davidschupauk <david_schupauk@hotmail.com>
2022-02-07 00:06:28 -0300

    Desafio 4

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,19 +1,41 @@
 import './NavBar.css'
+import Button from '../Button/Button'
+import CartWidget from '../CartWidget/CartWidget'
 
-const NavBar = () => {
+const NavBar = ({title, color}) => {
+  //Click en el boton Arg
+  const handleArg = () => {
+    console.log("ARGGGGG");
+  }
+  //Click en el boton America
+  const handleAme = () => {
+    console.log("America");
+  }
+  //Click en el boton Europa
+  const handleEuro = () => {
+    console.log("Eurooo");
+  }
+  //Click en el boton Seleccion
+  const handleSele = () => {
+    console.log("Seleee");
+  }
+
     return (
       <nav className='NavBar'>
         <div>
           <img src={'./images/logo192.png'} alt='logo' height="50px"/>
         </div>
         <div>
-            <h1 className="title">Camisetas del Mundo</h1>
+            <h1 className="title">{title}</h1>
         </div>
-        <button className='Option'>Argentina</button>
-        <button className='Option'>America</button>
-        <button className='Option'>Europa</button>
-        <button className='Option'>Selecciones</button>
+        
+        <Button label="Argentina" backgroundColor='red' handleClick={handleArg}/>
+        <Button label="America" colorText='green'handleClick={handleAme}/>
+        <Button label="Europa" handleClick={handleEuro}/>
+        <Button label="Selecciones" handleClick={handleSele}/>
+        <CartWidget />
       </nav>
+      
     )
 }
 
